0c49a2adc63ddeecbcf2b96922b29949e867c0b8
Trina <trinamceary@gmail.com>
2020-02-20 13:32:39 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+'use stricl';
+
+const apiKey = 'fAvwKXa8AAXHrlhhywnH2TgzzE4pSjTjhpZ1QPwc';
+const searchUrl = 'https://developer.nps.gov/api/v1/alerts';
+
+
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+  return queryItems.join('&');
+}
+
+function getNatlParks(query, maxResults=10) {
+  const params = {
+    key: apiKey,
+    q: query,
+    part: 'somethinghere?',
+    maxResults,
+    type: 'somethinghere?'
+  };
+  const queryString = formatQueryParams(params)
+  const url = searchURL + '?' + queryString;
+
+  console.log(url);
+
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson))
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
+}
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    const searchTerm = $('#js-search-state').val();
+    const maxResults = $('#js-max-results').val();
+    getNatlParks(searchTerm, maxResults);
+  });
+}
